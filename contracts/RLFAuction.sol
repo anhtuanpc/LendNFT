bd17248b690e35d2d0784cce5c56f742de512e5d
Anh Tuan <tuanda1@vmodev.com>
2023-03-09 17:16:33 +0700

    split contract

---
+++ contracts/RLFAuction.sol
@@ -0,0 +1,224 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract RLFMarketplace is Ownable, ReentrancyGuard, IERC721Receiver {
+    struct AuctionNFT {
+        address nft;
+        uint256 tokenId;
+        address creator;
+        address payToken;
+        uint256 initialPrice;
+        uint256 minBid;
+        uint256 startTime;
+        uint256 endTime;
+        address lastBidder;
+        uint256 heighestBid;
+        address winner;
+        bool success;
+    }
+
+    mapping(address => bool) private payableToken;
+    address[] private tokens;
+
+    // nft => tokenId => acuton struct
+    mapping(address => mapping(uint256 => AuctionNFT)) private auctionNfts;
+
+    // auciton index => bidding counts => bidder address => bid price
+    mapping(uint256 => mapping(uint256 => mapping(address => uint256)))
+        private bidPrices;
+
+    // events
+    event CreatedAuction(
+        address indexed nft,
+        uint256 indexed tokenId,
+        address payToken,
+        uint256 price,
+        uint256 minBid,
+        uint256 startTime,
+        uint256 endTime,
+        address indexed creator
+    );
+    event PlacedBid(
+        address indexed nft,
+        uint256 indexed tokenId,
+        address payToken,
+        uint256 bidPrice,
+        address indexed bidder
+    );
+
+    event ResultedAuction(
+        address indexed nft,
+        uint256 indexed tokenId,
+        address creator,
+        address indexed winner,
+        uint256 price,
+        address caller
+    );
+
+    modifier isAuction(address _nft, uint256 _tokenId) {
+        AuctionNFT memory auction = auctionNfts[_nft][_tokenId];
+        require(
+            auction.nft != address(0) && !auction.success,
+            "auction already created"
+        );
+        _;
+    }
+
+    modifier isNotAuction(address _nft, uint256 _tokenId) {
+        AuctionNFT memory auction = auctionNfts[_nft][_tokenId];
+        require(
+            auction.nft == address(0) || auction.success,
+            "auction already created"
+        );
+        _;
+    }
+
+    function createAuction(
+        address _nft,
+        uint256 _tokenId,
+        address _payToken,
+        uint256 _price,
+        uint256 _minBid,
+        uint256 _startTime,
+        uint256 _endTime
+    ) external isNotAuction(_nft, _tokenId) {
+        IERC721 nft = IERC721(_nft);
+        require(nft.ownerOf(_tokenId) == msg.sender, "not nft owner");
+        require(_endTime > _startTime, "invalid end time");
+
+        nft.transferFrom(msg.sender, address(this), _tokenId);
+
+        auctionNfts[_nft][_tokenId] = AuctionNFT({
+            nft: _nft,
+            tokenId: _tokenId,
+            creator: msg.sender,
+            payToken: _payToken,
+            initialPrice: _price,
+            minBid: _minBid,
+            startTime: _startTime,
+            endTime: _endTime,
+            lastBidder: address(0),
+            heighestBid: _price,
+            winner: address(0),
+            success: false
+        });
+
+        emit CreatedAuction(
+            _nft,
+            _tokenId,
+            _payToken,
+            _price,
+            _minBid,
+            _startTime,
+            _endTime,
+            msg.sender
+        );
+    }
+
+    function cancelAuction(
+        address _nft,
+        uint256 _tokenId
+    ) external isAuction(_nft, _tokenId) {
+        AuctionNFT memory auction = auctionNfts[_nft][_tokenId];
+        require(auction.creator == msg.sender, "not auction creator");
+        require(block.timestamp < auction.startTime, "auction already started");
+        require(auction.lastBidder == address(0), "already have bidder");
+
+        IERC721 nft = IERC721(_nft);
+        nft.transferFrom(address(this), msg.sender, _tokenId);
+        delete auctionNfts[_nft][_tokenId];
+    }
+
+    function placeBid(
+        address _nft,
+        uint256 _tokenId,
+        uint256 _bidPrice
+    ) external isAuction(_nft, _tokenId) nonReentrant {
+        require(
+            block.timestamp >= auctionNfts[_nft][_tokenId].startTime,
+            "auction not start"
+        );
+        require(
+            block.timestamp <= auctionNfts[_nft][_tokenId].endTime,
+            "auction ended"
+        );
+        require(
+            _bidPrice >=
+                auctionNfts[_nft][_tokenId].heighestBid +
+                    auctionNfts[_nft][_tokenId].minBid,
+            "less than min bid price"
+        );
+
+        AuctionNFT storage auction = auctionNfts[_nft][_tokenId];
+        IERC20 payToken = IERC20(auction.payToken);
+        payToken.transferFrom(msg.sender, address(this), _bidPrice);
+
+        if (auction.lastBidder != address(0)) {
+            address lastBidder = auction.lastBidder;
+            uint256 lastBidPrice = auction.heighestBid;
+
+            // Transfer back to last bidder
+            payToken.transfer(lastBidder, lastBidPrice);
+        }
+
+        // Set new heighest bid price
+        auction.lastBidder = msg.sender;
+        auction.heighestBid = _bidPrice;
+
+        emit PlacedBid(_nft, _tokenId, auction.payToken, _bidPrice, msg.sender);
+    }
+
+    function completeBid(address _nft, uint256 _tokenId) external nonReentrant {
+        require(!auctionNfts[_nft][_tokenId].success, "already resulted");
+        require(
+            msg.sender == owner() ||
+                msg.sender == auctionNfts[_nft][_tokenId].creator ||
+                msg.sender == auctionNfts[_nft][_tokenId].lastBidder,
+            "not creator, winner, or owner"
+        );
+        require(
+            block.timestamp > auctionNfts[_nft][_tokenId].endTime,
+            "auction not ended"
+        );
+
+        AuctionNFT storage auction = auctionNfts[_nft][_tokenId];
+        IERC20 payToken = IERC20(auction.payToken);
+        IERC721 nft = IERC721(auction.nft);
+
+        auction.success = true;
+        auction.winner = auction.creator;
+
+        uint256 heighestBid = auction.heighestBid;
+        uint256 totalPrice = heighestBid;
+
+        // Transfer to auction creator
+        payToken.transfer(auction.creator, totalPrice);
+
+        // Transfer NFT to the winner
+        nft.transferFrom(address(this), auction.lastBidder, auction.tokenId);
+
+        emit ResultedAuction(
+            _nft,
+            _tokenId,
+            auction.creator,
+            auction.lastBidder,
+            auction.heighestBid,
+            msg.sender
+        );
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
